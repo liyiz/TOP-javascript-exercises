084981cf4e57d3b786adfffc7b186a9597184b41
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-20 14:42:02 -0500

    03_reverseString: Initial attempt

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(stringInput) {
+    // get length of stringInput
+    // start from end of string array
+    // add to a temp array
+    // print out temp array
+    let tempString = '';
+    const stringSize = stringInput.length;
+    // hello 0,1,2,3,4
+    // stringSize = 5
+    for (let i = 1; i < stringSize+1; i++) {
+        const reversedIndex = stringSize - i;
+        tempString += stringInput[reversedIndex];
+    }
+    return tempString;
 };
 
 // Do not edit below this line
