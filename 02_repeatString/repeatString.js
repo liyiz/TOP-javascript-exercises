99ed60ba8dceaac456bd8df040fe7d930aecbaef
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-20 12:56:00 -0500

    Initial attempt at 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(givenString, timesToRepeat) {
+    let stringToPrint = '';
+    for (let i = 0; i < timesToRepeat; i++ ) {
+        stringToPrint += givenString;
+    }
+    return stringToPrint;
 };
 
 // Do not edit below this line
