2231ae516671ca1927766145042555b5d82e51ba
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-21 14:18:00 -0500

    04_removeFromArray: Clean up unused code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,22 +2,6 @@ const removeFromArray = function(inputArray, ...toRemove) {
     // create array from input arguments "toRemove"
     let args = Array.from(toRemove);
     
-    // temporary array to collect indexes of items to be removed from inputArray
-    let indexesToRemove = [];
-    // compare inputArray and args
-    for (let i = 0; i < args.length; i++) {
-
-        for (let j = 0; j < inputArray.length; j++) {
-            if (args[i] === inputArray[j]) {
-                // save positions of items to remove
-                indexesToRemove.push(j);
-            }
-        }
-
-    }
-    
-    console.log(indexesToRemove);
-
     // remove the items
     const newArray = inputArray.filter((item) => !args.includes(item))
 
