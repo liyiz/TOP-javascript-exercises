aafe9e1db294b8d505916a2ce2e186bb72072f57
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-21 14:23:57 -0500

    04_removeFromArray: Remove redundant code
    Spread operator, rest parameters, and arguments in a function are already an array-like object, so the use of Array.from() is redundant.
    It's possible to directly reference toRemove and treat it like an array in this case.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,9 @@
 const removeFromArray = function(inputArray, ...toRemove) {
-    // create array from input arguments "toRemove"
-    let args = Array.from(toRemove);
     
+    let newArray = [];
     // remove the items
-    const newArray = inputArray.filter((item) => !args.includes(item))
+    return newArray = inputArray.filter((item) => !toRemove.includes(item));
 
-    // return the final desired array
-    return newArray;
 };
 
 // Do not edit below this line
