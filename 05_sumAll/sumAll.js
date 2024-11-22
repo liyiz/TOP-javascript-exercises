a98021e7779d6e72a08ceeb8395f902359e09de9
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-22 12:32:33 -0500

    05_sumAll: Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (isNaN(a) || 
+        isNaN(b) || 
+        !Number.isInteger(a) || 
+        !Number.isInteger(b) ||
+        Math.sign(a) === -1  ||
+        Math.sign(b) === -1
+    ) {
+        return 'ERROR';
+    }
 
+    let highNum = Math.max(a, b);
+    let lowNum = Math.min(a, b);
+    let sumTotal = 0;
+
+    for (let i = lowNum; i <= highNum; i++) {
+        sumTotal += i;
+    }
+
+    return sumTotal;
 };
 
 // Do not edit below this line
