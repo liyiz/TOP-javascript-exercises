aec8d4732d55256edfde3873788490b70a62e421
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-11-23 22:45:35 -0500

    07_tempConversion: Solved problem.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    // Formula: (F - 32) * 5/9 = C
+    const celsius = (fahrenheit - 32) * 5/9;
+    // Rounds up to nearest tenth, but ensures no decimal places if integer.
+    return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    // Formula: (C * (9/5)) + 32 = 32F
+    const fahrenheit = (celsius * (9/5)) + 32;
+    return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
