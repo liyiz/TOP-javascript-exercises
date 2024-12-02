033d633da0a82d873d628fc16f304fedecc86626
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-02 17:45:17 -0500

    08_calculator: complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total, num) => { return total += num; }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let total = 1;
+  for (let i = 0 ; i < power; i++) {
+    total *= num;
+  };
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for (let i = 0; i < num; i++) {
+    total *= (num - i);
+  };
+  return total;
 };
 
 // Do not edit below this line
