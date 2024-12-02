3f618a26360b08533f944023ece72e34ecbc9076
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-02 18:55:03 -0500

    09_palindromes: Complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,9 @@
 const palindromes = function (input) {
-
-    const array = Array.from(input);
-    // console.log(array);
-    //find if there are spaces
-    const hasSpaces = array.some(element => element === ' ' ? true : false);
-    // console.log(hasSpaces);
-    //split based on spaces
-    //find if there are commas
-    const hasCommas = array.some(element => element === ',' ? true : false);
-    // console.log(hasCommas);
-    //split based on commas
-
+    // Create array from input string and set it to all lowercase
+    // Filter array so that every character is alphabetical
+    const toTest = Array.from(input.toLowerCase()).filter((char) => /[0-9A-Za-z]/.test(char));
     //compare array and reversed array
-    const result = array.every((element, index) => element === array.reverse()[index]);
+    const result = toTest.every((element, index) => element === toTest.reverse()[index]);
     //if arrays match -> palindrome true
     return result;
 };
