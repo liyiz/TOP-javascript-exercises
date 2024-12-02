a5d341987c65527aa1d9a82b68da7b5198ae17fb
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-02 17:49:50 -0500

    09_palindromes: Add pseudocode

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
 const palindromes = function () {
+    //find if there are spaces
+    //split based on spaces
+    //find if there are commas
+    //split based on commas
 
+    //compare array and reversed array
+    //if arrays match -> palindrome true
 };
 
 // Do not edit below this line
