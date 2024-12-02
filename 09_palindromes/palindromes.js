52270f8911f8d1025623782a27ea6a3af36ef6dc
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-02 18:27:40 -0500

    09_palindromes: Implelement solution for first 2 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,20 @@
 const palindromes = function (input) {
 
     const array = Array.from(input);
-    console.log(array);
+    // console.log(array);
     //find if there are spaces
     const hasSpaces = array.some(element => element === ' ' ? true : false);
-    console.log(hasSpaces);
+    // console.log(hasSpaces);
     //split based on spaces
     //find if there are commas
     const hasCommas = array.some(element => element === ',' ? true : false);
-    console.log(hasCommas);
+    // console.log(hasCommas);
     //split based on commas
 
     //compare array and reversed array
+    const result = array.every((element, index) => element === array.reverse()[index]);
     //if arrays match -> palindrome true
+    return result;
 };
 
 // Do not edit below this line
