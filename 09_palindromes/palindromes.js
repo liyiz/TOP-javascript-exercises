c46bc59b0afc56a95ec63d1bacef2cb0c919565a
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-02 17:56:45 -0500

    09_palindromes: Add checks for spaces and commas

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,14 @@
-const palindromes = function () {
+const palindromes = function (input) {
+
+    const array = Array.from(input);
+    console.log(array);
     //find if there are spaces
+    const hasSpaces = array.some(element => element === ' ' ? true : false);
+    console.log(hasSpaces);
     //split based on spaces
     //find if there are commas
+    const hasCommas = array.some(element => element === ',' ? true : false);
+    console.log(hasCommas);
     //split based on commas
 
     //compare array and reversed array
