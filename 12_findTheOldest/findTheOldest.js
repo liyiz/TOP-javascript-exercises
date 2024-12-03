cc8a4c9905ca45adf7b2a5e42d141eb9dab71e71
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-03 11:56:08 -0500

    12_findTheOldest: Update solution
    Code passes other two tests in the exercise. Had an issue with how I thought .reduce() was handling data types, I didn't know that with array methods each element keeps its original form as it's being passed through the iteration. The array methods don't automatically know which property of an object you want to work with - they just pass along the whole thing - so it can inevitably end up comparing an object to just a number!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.sort((a, b) => {
+        // get current year
+        const currentYear = (new Date()).getFullYear();
+        let yearCapA = null;
+        let yearCapB = null;
 
+        // if yearOfDeath not available
+        // assign current date
+        if (a.yearOfDeath === undefined) {
+            yearCapA = currentYear;
+        } else {
+            yearCapA = a.yearOfDeath;
+        }
+        if (b.yearOfDeath === undefined) {
+            yearCapB = currentYear;
+        } else {
+            yearCapB = b.yearOfDeath;
+        }
+        
+        const lastPerson = yearCapA - a.yearOfBirth;
+        const nextPerson = yearCapB - b.yearOfBirth;
+        a.age = lastPerson;
+        b.age = nextPerson;
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    // passes the whole object (type) AND THEN
+    // compares each object's property (number to number)
+    // then picks a or b to keep in the newly made array.
+    .reduce((a, b) => a.age > b.age ? a : b); 
+
+    return oldest;
 };
 
 // Do not edit below this line
