8853a66cc210e9cae171459e1899876b2bf52b38
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-03 09:50:00 -0500

    10_fibonacci: Curr solution accepts single numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // num is max length
+    let fibonacci = [1, 1]; //add F1 and F2 of fibonacci sequence - outliers
+    let i = 2; // start looping on F3 of fibonacci sequence
+    
+    while (i < num) {
+        let sum = 0;
+        sum = fibonacci[i-1] + fibonacci[i-2]
+        fibonacci.push(sum);
+        // console.log(sum);
+        // console.log(fibonacci);
+        i++
+    };
+
+    return fibonacci[num-1];
 
 };
 
