d66daca9986af4a8185817614604942903e64e31
Liyi Z. <5160472+liyiz@users.noreply.github.com>
2024-12-03 09:56:27 -0500

    Add 0 index to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,9 @@
 const fibonacci = function(num) {
     // num is max length
-    let fibonacci = [1, 1]; //add F1 and F2 of fibonacci sequence - outliers
+    let fibonacci = [0, 1]; //add F0, F1 and F2 of fibonacci sequence - outliers
     let i = 2; // start looping on F3 of fibonacci sequence
     
-    while (i < num) {
+    while (i < num + 1) {
         let sum = 0;
         sum = fibonacci[i-1] + fibonacci[i-2]
         fibonacci.push(sum);
@@ -12,7 +12,7 @@ const fibonacci = function(num) {
         i++
     };
 
-    return fibonacci[num-1];
+    return fibonacci[num];
 
 };
 
